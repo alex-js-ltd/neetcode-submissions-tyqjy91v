c9b8647ef4f759c39a4015c8faef41fb9d6540cc
alex-js-ltd <109146897+alex-js-ltd@users.noreply.github.com>
2026-04-15 09:55:28 +0100

    Add: string-encode-and-decode - submission-10

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-10.js
@@ -0,0 +1,45 @@
+function Solution () {};
+
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+Solution.prototype.encode = function (strs) {
+
+    let str = '';
+
+    for(const word of strs) {
+        str += word.length + '#' + word;
+    }
+
+    return str;
+};
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+Solution.prototype.decode = function (str) {
+    let res = [];
+    
+    let i = 0;
+
+    while(i < str.length) {
+
+        const hashIndex = str.indexOf('#', i);
+
+        const len = Number(str.substring(i, hashIndex));
+
+        const start = hashIndex + 1;
+
+        const end = start + len;
+
+        const word = str.substring(start, end);
+
+        res.push(word);
+
+        i = end;
+    }
+
+    return res;
+};
