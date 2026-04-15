0f22cca8c6fdfe2e63dcb86230c6bc281d8df3f4
alex-js-ltd <109146897+alex-js-ltd@users.noreply.github.com>
2026-04-15 09:39:15 +0100

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.js
@@ -0,0 +1,26 @@
+function Solution () {};
+
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+
+Solution.prototype.twoSum = function (nums, target) {
+
+    const seen = new Map();
+
+    for(let i = 0; i <nums.length; i ++) {
+
+        const need = target - nums[i];
+
+        if(seen.has(need)) {
+            return [i, seen.get(need)];
+        } else {
+            seen.set(nums[i], i);
+        }
+    }
+
+    return [];
+
+};
