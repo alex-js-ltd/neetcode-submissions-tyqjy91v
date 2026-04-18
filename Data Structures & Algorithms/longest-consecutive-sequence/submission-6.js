f92327b48132e69de67f7a8ef20af29f02c74ddf
alex-js-ltd <109146897+alex-js-ltd@users.noreply.github.com>
2026-04-18 10:00:42 +0100

    Add: longest-consecutive-sequence - submission-6

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-6.js
@@ -0,0 +1,29 @@
+
+function Solution () {};
+
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+Solution.prototype.longestConsecutive = function (nums) {
+
+    const seen = new Set(nums);
+    let longest = 0;
+
+    for(const n of nums) {
+        
+       
+        if(!seen.has(n - 1)) {
+            let length = 1;
+
+            while(seen.has(n + length)) {
+                length += 1;
+            }
+
+            longest = Math.max(longest, length);
+        }
+    }
+
+    return longest;
+
+};
