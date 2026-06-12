52d135d90ef54f70905bcbc0466cef573990b578
alex-js-ltd <109146897+alex-js-ltd@users.noreply.github.com>
2026-06-12 12:34:37 +0100

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.js
@@ -0,0 +1,40 @@
+function Solution () {};
+
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+Solution.prototype.isPalindrome = function (s) {
+
+    let l = 0;
+    let r = s.length -1;
+
+    while (l < r) {
+
+        if(l < r && !this.isAlpha(s[l])) {
+            l += 1;
+            continue;
+        }
+
+        if(l < r && !this.isAlpha(s[r])) {
+            r -= 1;
+            continue;
+        }
+
+        const left = s[l].toLowerCase();
+        const right = s[r].toLowerCase();
+
+        if(left !== right) {
+            return false;
+        }
+
+        l += 1;
+        r -= 1;
+    }
+
+    return true;
+};
+
+Solution.prototype.isAlpha = function (char) {
+    return (char >= 'a' &&  char <= 'z' || char >= 'A' &&  char <= 'Z' || char >= '0' &&  char <= '9');
+};
