bd42c33e8d730401221493a1c077b2e1d9af78f3
alex-js-ltd <109146897+alex-js-ltd@users.noreply.github.com>
2026-06-17 10:44:41 +0100

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.js
@@ -0,0 +1,51 @@
+
+
+function Solution () {};
+
+
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+Solution.prototype.characterReplacement = function (s, k) {
+    
+    const window = new Map();
+
+    let l = 0;
+
+    let longest = 0;
+
+    let maxFreq = 0;
+
+    for(let r = 0; r < s.length; r++) {
+
+        const char = s[r];
+
+        if(!window.has(char)) {
+            window.set(char, 0);
+        }
+
+        const prev = window.get(char);
+        const next = prev + 1;
+
+        window.set(char, next);
+
+        maxFreq = Math.max(maxFreq, next);
+
+        const windowLength = () => r - l + 1;
+        
+        while(windowLength() - maxFreq > k) {
+            window.set(s[l], window.get(s[l]) - 1);
+            l += 1;
+        }
+
+
+        longest = Math.max(longest,  windowLength());
+
+
+    }
+
+    return longest;
+
+};
