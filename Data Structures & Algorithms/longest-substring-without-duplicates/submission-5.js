82a026721b721492a3d7f1a9fcda508d78148ceb
alex-js-ltd <109146897+alex-js-ltd@users.noreply.github.com>
2026-06-18 12:19:52 +0100

    Add: longest-substring-without-duplicates - submission-5

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-5.js
@@ -0,0 +1,32 @@
+function Solution () {};
+
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+Solution.prototype.lengthOfLongestSubstring = function (s) {
+
+    const window = new Set ();
+
+    let l = 0;
+
+    let longest = 0;
+
+    for(let r = 0; r < s.length; r++) {
+
+        // Remove duplicates
+        while(window.has(s[r])) {
+            window.delete(s[l]);
+            l += 1;
+        }
+
+        window.add(s[r]);
+
+        const length = r - l + 1;
+
+        longest = Math.max(longest, length);
+
+    }
+
+    return longest;
+};
