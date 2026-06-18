96a20c15d2d294c7dc31bd5811b24ecc7a3906ce
alex-js-ltd <109146897+alex-js-ltd@users.noreply.github.com>
2026-06-18 15:04:12 +0100

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.js
@@ -0,0 +1,24 @@
+
+function Solution () {};
+
+  /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+Solution.prototype.hasDuplicate = function (nums) {
+
+    const seen = new Set ();
+
+
+    for(const n of nums) {
+
+        if(seen.has(n)) {
+            return true;
+        }
+
+        seen.add(n);
+    }
+
+    return false;
+
+};
