f36c4ae2fc7d66e9ccac7ee59a5fe029a0bf09ef
alex-js-ltd <109146897+alex-js-ltd@users.noreply.github.com>
2026-06-21 13:11:26 +0100

    Add: top-k-elements-in-list - submission-7

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-7.js
@@ -0,0 +1,45 @@
+function Solution () {};
+
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+Solution.prototype.topKFrequent = function (nums, k) {
+
+    const seen = new Map();
+
+    for(const n of nums) {
+
+        if(!seen.has(n)) {
+            seen.set(n, 0);
+        }
+
+        const freq = seen.get(n) + 1;
+
+        seen.set(n, freq);
+    }
+
+    const buckets = new Array(nums.length + 1).fill().map(() => {
+        return [];
+    });
+
+    for(const [num, count] of seen.entries()) {
+        buckets[count].push(num);
+    }
+
+    const res = [];
+
+    for(let i = buckets.length -1; i >= 0; i--) {
+
+        for(const n of buckets[i]) {
+            res.push(n);
+        }
+
+        if(res.length === k) {
+            return res;
+        }
+    }
+
+
+};
