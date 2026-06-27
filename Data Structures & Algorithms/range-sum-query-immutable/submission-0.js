e3125da0150224685e82adec93010e0f0272fdbc
alex-js-ltd <109146897+alex-js-ltd@users.noreply.github.com>
2026-06-27 11:17:12 +0100

    Add: range-sum-query-immutable - submission-0

---
+++ Data Structures & Algorithms/range-sum-query-immutable/submission-0.js
@@ -0,0 +1,23 @@
+  /**
+     * @param {number[]} nums
+     */
+function NumArray (nums) {
+    this.nums = nums;
+};
+
+
+    /**
+     * @param {number} left
+     * @param {number} right
+     * @return {number}
+     */
+NumArray.prototype.sumRange = function (left,right) {
+
+    let sum = 0;
+
+    for(let i = left; i <= right; i++) {
+        sum += this.nums[i];
+    }
+
+    return sum;
+};
