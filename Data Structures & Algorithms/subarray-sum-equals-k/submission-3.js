eaf91f46c537fc7992d900b91b6785f41f798192
alex-js-ltd <109146897+alex-js-ltd@users.noreply.github.com>
2026-06-28 10:25:27 +0100

    Add: subarray-sum-equals-k - submission-3

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-3.js
@@ -0,0 +1,39 @@
+function Solution () {};
+
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+Solution.prototype.subarraySum = function (nums, k) {
+
+
+    const seen = new Map();
+
+    seen.set(0, 1);
+
+    let count = 0;
+
+    let prefix = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+
+        prefix += nums[i];
+
+        const need = prefix - k;
+
+        if(seen.has(need)) {
+            count += seen.get(need);
+        }
+
+        if(!seen.has(prefix)) {
+            seen.set(prefix, 0);
+        }
+
+        const freq = seen.get(prefix) + 1;
+        seen.set(prefix, freq);
+
+    }
+
+    return count;
+};
