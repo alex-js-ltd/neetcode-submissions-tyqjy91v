4680377eca73c1030fb54364b7c47b516efe562d
alex-js-ltd <109146897+alex-js-ltd@users.noreply.github.com>
2026-07-01 11:14:48 +0100

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.js
@@ -0,0 +1,56 @@
+function Solution () {};
+
+    /**
+     * @param {character[][]} grid
+     * @return {number}
+     */
+Solution.prototype.numIslands = function (grid) {
+
+    if(grid == null) {
+        return 0;
+    }
+
+    if(grid.length === 0) {
+        return 0;
+    }
+
+    const rows = grid.length;
+    const cols = grid[0].length;
+
+    let count = 0;
+
+    function dfs(r, c) {
+
+        // Out of bounds
+        if(r < 0 || r >= rows || c < 0 || c >= cols) {
+            return;
+        }
+
+        // Water
+        if(grid[r][c] === '0') {
+            return;
+        }
+
+        if(grid[r][c] === '1') {
+            grid[r][c] = '0';
+        }
+
+        dfs(r, c +1);
+        dfs(r, c -1);
+        dfs(r +1, c);
+        dfs(r -1, c);
+    }
+
+    for(let r = 0; r < rows; r++) {
+
+        for(let c = 0; c < cols; c++) {
+
+           if(grid[r][c] === '1') {
+                count += 1;
+           }
+           dfs(r, c);
+        }
+    }
+
+    return count;
+};
